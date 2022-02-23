e03be0c2daaf8e960ae8bbdec3160dc79838c512
jwhong <jwhong98@gmail.com>
2022-02-23 09:11:48 -0800

    mobile nav

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,7 +1,20 @@
 import React from "react";
+import logo from "../../images/logo.svg";
+import mobile from "../../images/icon-hamburger.svg";
+import { Nav, NavContent, Logo, MobileIcon } from "./NavbarElements";
 
 const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <Nav>
+      <NavContent>
+        <Logo src={logo} />
+        {/* NavMenu
+            Button
+         */}
+      </NavContent>
+      <MobileIcon src={mobile} />
+    </Nav>
+  );
 };
 
 export default Navbar;
---
+++ src/components/Navbar/NavbarElements.js
@@ -0,0 +1,16 @@
+import styled from "styled-components";
+
+export const Nav = styled.nav`
+  width: 100%;
+  height: 80px;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  padding: 0 1.5rem;
+`;
+
+export const NavContent = styled.div``;
+
+export const Logo = styled.img``;
+
+export const MobileIcon = styled.img``;
